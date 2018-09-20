20961bd29f3481a1b9653899f4a6a1ab84d2dfa5
jjshawshaw <jshaw00@stuy.edu>
2018-09-19 21:22:26 -0400

    Added testing function as main

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,7 +1,16 @@
-public double celsiusToFahrenheit(double cel) {
-  return (9 * cel / 5) + 32
-}
+public class CtoFTester {
 
-public double fahrenheitToCelsius(double cel) {
-  return (cel - 32) * 5 / 9
+  public static double celsiusToFahrenheit(double cel) {
+    return (9 * cel / 5) + 32;
+  }
+
+  public static double fahrenheitToCelsius(double cel) {
+    return (cel - 32) * 5 / 9;
+  }
+
+  public static void main(String[] args){
+    if ((celsiusToFahrenheit(0) == 32) && (fahrenheitToCelsius(32) == 0)) {
+        System.out.println("Functions work");
+    }
+  }
 }
